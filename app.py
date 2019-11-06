bb08757aee65ea74b2e6868a5fd2e047862c513f
Carlos Villa <carloslorenzovilla@gmail.com>
2019-11-05 18:31:22 -0800

    Change from list to dictionary

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ Created on Mon Nov  4 10:27:38 2019
 from itertools import permutations
 import sys
 
-def build_list():
-    """ Builds a list of words from a text file.
+def build_dict():
+    """ Builds a list of words in a text file.
     
         Assumption: Text file line format is <word>\r\n
         
@@ -17,42 +17,21 @@ def build_list():
         returns: list    
     """
     fin = open('words.txt')
-    list_of_words = []
+    dict_of_words = {}
     
     for line in fin:
         word = line.strip()
-        list_of_words.append(word)
-    return list_of_words
-
-def in_bisect(words, target):
-    """ Determines if element is present in sorted list using
-        a binary search. Uses recursion.
-        
-        words: list
-        
-        returns: boolean
-    """
-    if len(words) == 0:
-        return False
+        dict_of_words[word] = 1
+    return dict_of_words
     
-    middle = len(words)//2
-    
-    if words[middle] == target:
-        return True
-    
-    if target < words[middle]:
-        return in_bisect(words[:middle], target)
-    else:
-        return in_bisect(words[middle+1:], target)
-    
-def generate_words(dictionary, in_bisect):
+def generate_words(dictionary):
     """ Generates a list of possible words of a
         specific size given a string of letters.
         Permutations are compared with elements in
         a dictionary. Throws ValueError exception
         if size is not an integer.
         
-        dictionary: list
+        dictionary: dict
         
         returns: list
     """
@@ -71,7 +50,7 @@ def generate_words(dictionary, in_bisect):
     for word in permutations(letters, size):
         delimeter = ""
         new_word = delimeter.join(word)
-        if in_bisect(dictionary, new_word) and new_word not in possible_words:
+        if new_word in dictionary:
             possible_words.append(new_word)  
         
     return possible_words
@@ -89,11 +68,11 @@ def display(word_list):
 
 
 def main():
-    dictionary = build_list()
+    dictionary = build_dict()
 
     while True:
         
-        display(generate_words(dictionary, in_bisect))
+        display(generate_words(dictionary))
         print('\n')
         if input('Try again? (y/n): ') == 'y':
             continue
